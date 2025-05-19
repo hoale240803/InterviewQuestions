2545d4e790f18001dda589253467e47fb0bd76f9
HoaLe <hoale240803@gmail.com>
2025-05-19 17:55:29 +0700

    Sioux interview question | CalculateTotalBurnableCandle

---
+++ Candle.cs
@@ -0,0 +1,25 @@
+namespace InterviewQuestions
+{
+    public class Candle
+    {
+        public static int CalculateTotalBurnableCandle(int initialCandles, int leftoversPerCandle)
+        {
+            int totalCandlesBurned = 0;
+            int candles = initialCandles;
+            int leftovers = 0;
+
+            while (candles > 0)
+            {
+                // Burn all available candles. Each burned candle produces 1 leftover.
+                totalCandlesBurned += candles;
+                leftovers += candles;
+
+                // Create new candles from leftovers.
+                candles = leftovers / leftoversPerCandle;
+                leftovers = leftovers % leftoversPerCandle;
+            }
+
+            return totalCandlesBurned;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using InterviewQuestions;
+
+// Input: n=5, k=2
+// Output: 9
+
+Console.WriteLine(Candle.CalculateTotalBurnableCandle(5, 2));
+
+// Input: n=2, k=2
+// Output: 3
+Candle c2 = new Candle();
+Console.WriteLine(Candle.CalculateTotalBurnableCandle(2, 2));
+
+// Input: n=1, k=1
+// Output: infinite loop 
+// Candle c3 = new Candle();
+// Console.WriteLine(Candle.CalculateTotalBurnableCandleV2(1, 1));
